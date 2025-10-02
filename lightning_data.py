672bab19e5fa9812849223130d6dc0530dfcd86f
Copticoder <ahmed.abdelsalam.ap@gmail.com>
2025-10-02 14:57:08 +0000

    increasing length sampler dl

--- lightning_data.py
+++ lightning_data.py
@@ -1,10 +1,22 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Sampler
 from torchdata.datapipes.map import MapDataPipe
 from pytorch_lightning import LightningDataModule
 import warnings
 from datasets import load_dataset
 
 warnings.filterwarnings("ignore", ".*does not have many workers.*")
+
+
+class LengthSortedSampler(Sampler[int]):
+    def __init__(self, lengths, ascending: bool = True):
+        self.lengths = lengths
+        self.indices = sorted(range(len(lengths)), key=lambda i: lengths[i], reverse=not ascending)
+
+    def __iter__(self):
+        return iter(self.indices)
+
+    def __len__(self):
+        return len(self.indices)
 
 
 class TranslationDataModule(LightningDataModule):
@@ -16,12 +28,15 @@ class TranslationDataModule(LightningDataModule):
         dataset_config_name,
         source_lang,
         target_lang,
+        sort_by_length: bool = True,
+        sort_direction: str = "asc",
     ):
         super().__init__()
         self.save_hyperparameters(ignore="tokenizer")
         self.tokenizer = tokenizer
         self.train_data = None
         self.val_data = None
+        self._train_sampler = None
 
     def setup(self, stage):
         prompts = load_dataset(
@@ -30,7 +45,20 @@ class TranslationDataModule(LightningDataModule):
         self.train_data = TranslationDataPipe(prompts["train"], self.tokenizer, src_col="sentence_" + self.hparams.source_lang, tgt_col="sentence_" + self.hparams.target_lang)
         self.val_data = TranslationDataPipe(prompts["valid"], self.tokenizer, src_col="sentence_" + self.hparams.source_lang, tgt_col="sentence_" + self.hparams.target_lang)
 
+        # Optional: build a sampler to iterate by increasing source length
+        if self.hparams.sort_by_length:
+            src_col = "sentence_" + self.hparams.source_lang
+            # Compute source lengths once
+            lengths = [
+                len(self.tokenizer(self.train_data.prompts[i][src_col]).input_ids)  # type: ignore[attr-defined]
+                for i in range(len(self.train_data))
+            ]
+            ascending = (self.hparams.sort_direction.lower() != "desc")
+            self._train_sampler = LengthSortedSampler(lengths, ascending=ascending)
+
     def train_dataloader(self):
+        if self._train_sampler is not None:
+            return DataLoader(self.train_data, sampler=self._train_sampler, batch_size=None, num_workers=0)
         return DataLoader(self.train_data, shuffle=True, batch_size=None, num_workers=0)
 
     def val_dataloader(self):
